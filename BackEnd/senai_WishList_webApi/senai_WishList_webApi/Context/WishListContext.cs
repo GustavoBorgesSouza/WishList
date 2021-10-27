fe0f3709aa0b859a3a3bfa3d1238c43b3e6222c6
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2021-10-27 11:57:31 -0300

    Alterações no CSS

--- BackEnd/senai_WishList_webApi/senai_WishList_webApi/Context/WishListContext.cs
+++ BackEnd/senai_WishList_webApi/senai_WishList_webApi/Context/WishListContext.cs
@@ -25,8 +25,11 @@ namespace senai_WishList_webApi.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=NOTE0113F3\\SQLEXPRESS; Initial Catalog=WishList; integrated security=true;");
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //  optionsBuilder.UseSqlServer("Data Source=NOTE0113F3\\SQLEXPRESS; Initial Catalog=WishList; integrated security=true;");
+
+                // Conexão do Rezende
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-Q4CU27L\\SQLEXPRESS; initial catalog=WishList; user Id=sa; pwd=senai@132;");
             }
         }
 
